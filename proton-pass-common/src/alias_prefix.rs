0c47fc592f33f248ca09dde5b1f2069821571950
Carlos Quintana <carlos.quintana@proton.ch>
2023-09-18 13:35:06 +0200

    feat: add prefix alias validation

---
+++ proton-pass-common/src/alias_prefix.rs
@@ -0,0 +1,38 @@
+pub enum AliasPrefixError {
+    TwoConsecutiveDots,
+    InvalidCharacter,
+    DotAtTheEnd,
+    PrefixTooLong,
+    PrefixEmpty,
+}
+
+pub const MAX_PREFIX_LENGTH: usize = 40;
+
+pub fn validate_alias_prefix(prefix: &str) -> Result<(), AliasPrefixError> {
+    if prefix.is_empty() {
+        return Err(AliasPrefixError::PrefixEmpty);
+    }
+
+    if prefix.len() >= MAX_PREFIX_LENGTH {
+        return Err(AliasPrefixError::PrefixTooLong);
+    }
+
+    if prefix.contains("..") {
+        return Err(AliasPrefixError::TwoConsecutiveDots);
+    }
+
+    if prefix.ends_with('.') {
+        return Err(AliasPrefixError::DotAtTheEnd);
+    }
+
+    let valid_characters = [
+        '_', '-', '.', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    ];
+
+    if !prefix.chars().all(|c| valid_characters.contains(&c)) {
+        Err(AliasPrefixError::InvalidCharacter)
+    } else {
+        Ok(())
+    }
+}
--- proton-pass-mobile/src/lib.rs
+++ proton-pass-mobile/src/lib.rs
@@ -1,3 +1,3 @@
 pub fn is_email_valid(email: String) -> bool {
-    proton_pass_common::is_email_valid(&email)
+    proton_pass_common::email::is_email_valid(&email)
 }

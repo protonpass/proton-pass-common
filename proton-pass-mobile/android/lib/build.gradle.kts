97c95020e2602c687e76a0486bd25f2421a0244b
Carlos Quintana <carlos.quintana@proton.ch>
2023-09-20 15:24:29 +0200

    feat: build android aar

---
+++ proton-pass-mobile/android/lib/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "proton.android.pass.commonrust"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            "proguard-rules.pro"
+        )
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    val COROUTINES = "1.6.4"
+    val JNA = "5.13.0"
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${COROUTINES}")
+    implementation("net.java.dev.jna:jna:${JNA}")
+
+
+    testImplementation("junit:junit:4.13.2")
+}

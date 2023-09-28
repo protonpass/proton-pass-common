4aa12dd2a52a715737b1c520c7e7199ab3859f26
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-09-28 14:16:40 +0200

    feat: sanitized edited URI

--- proton-pass-common/src/totp/sanitizer.rs
+++ proton-pass-common/src/totp/sanitizer.rs
@@ -1,10 +1,8 @@
-use crate::totp::algorithm::Algorithm;
-use crate::totp::algorithm::Algorithm::SHA1;
-use crate::totp::components::TOTPComponents;
-
-pub const DEFAULT_ALGORITHM: Algorithm = SHA1;
-pub const DEFAULT_DIGITS: u8 = 6;
-pub const DEFAULT_PERIOD: u16 = 30;
+use crate::totp::components::{
+    TOTPComponents, DEFAULT_ALGORITHM, DEFAULT_DIGITS, DEFAULT_PERIOD, OTP_SCHEME, QUERY_ALGORITHM, QUERY_DIGITS,
+    QUERY_PERIOD, QUERY_SECRET, TOTP_HOST,
+};
+use url::Url;
 
 /// Take an original URI string and convert it to a string for user to edit.
 ///
@@ -29,23 +27,66 @@ pub fn uri_for_editing(original_uri: &str) -> String {
     original_uri_string
 }
 
-impl TOTPComponents {
-    fn has_default_params(&self) -> bool {
-        let default_algorithm = match &self.algorithm {
-            Some(value) => *value == DEFAULT_ALGORITHM,
-            _ => true,
-        };
+/// Sanitize the user input URI before saving.
+///
+/// - Invalid
+///   => Return as-is
+///
+/// - Valid with no params
+///   => Add default params
+///
+/// - Valid with default params
+///   => Return as-is
+///
+/// - Valid with custom params
+///   => Return as-is
+pub fn uri_for_saving(edited_uri: &str) -> String {
+    let edited_uri_string = edited_uri.to_string();
 
-        let default_digits = match &self.digits {
-            Some(value) => *value == DEFAULT_DIGITS,
-            _ => true,
-        };
-
-        let default_period = match &self.period {
-            Some(value) => *value == DEFAULT_PERIOD,
-            _ => true,
-        };
-
-        default_algorithm && default_digits && default_period
+    let components;
+    if let Ok(value) = TOTPComponents::from_uri(edited_uri) {
+        components = value
+    } else {
+        // Invalid URI => return as-is
+        return edited_uri_string;
     }
+
+    let base_uri = format!("{}://{}/", OTP_SCHEME, TOTP_HOST);
+    let mut uri;
+    if let Ok(value) = Url::parse(&base_uri) {
+        uri = value
+    } else {
+        return edited_uri_string;
+    }
+
+    // Add label path
+    if let Some(label) = components.label {
+        uri.set_path(label.as_str());
+    }
+
+    // Set secret query
+    uri.query_pairs_mut().append_pair(QUERY_SECRET, &components.secret);
+
+    // Set algorithm query
+    let algorithm = match components.algorithm {
+        Some(entered_algorithm) => entered_algorithm,
+        _ => DEFAULT_ALGORITHM,
+    };
+    uri.query_pairs_mut().append_pair(QUERY_ALGORITHM, algorithm.value());
+
+    // Set digits
+    let digits = match components.digits {
+        Some(entered_digits) => entered_digits,
+        _ => DEFAULT_DIGITS,
+    };
+    uri.query_pairs_mut().append_pair(QUERY_DIGITS, &format!("{}", digits));
+
+    // Set period
+    let period = match components.period {
+        Some(entered_period) => entered_period,
+        _ => DEFAULT_PERIOD,
+    };
+    uri.query_pairs_mut().append_pair(QUERY_PERIOD, &format!("{}", period));
+
+    uri.as_str().to_string()
 }

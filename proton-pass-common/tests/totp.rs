1f7eb0ecf472996a27302b42faeb87f6612c091c
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-09-29 09:25:27 +0200

    tests: add more tests for saving URI

--- proton-pass-common/tests/totp.rs
+++ proton-pass-common/tests/totp.rs
@@ -44,12 +44,67 @@ fn for_editing() {
 
 #[test]
 fn for_saving() {
-    // assert_eq!(uri_for_saving("", ""), "");
-
+    // Invalid original, edit with secret only
+    // => sanitize secret and add default params
     assert_eq!(
         uri_for_saving("invalid original", " some secret "),
         "otpauth://totp/?secret=somesecret&algorithm=SHA1&digits=6&period=30"
     );
 
-    // assert_eq!(uri_for_saving("invalid original", "otpauth://totp/?secret=edited_secret"), "invalid edited");
+    // Invalid original, edit with valid URI
+    // => save the edited URI as-is
+    assert_eq!(
+        uri_for_saving(
+            "invalid original",
+            "otpauth://totp/?secret=somesecret&algorithm=SHA1&digits=6&period=30"
+        ),
+        "otpauth://totp/?secret=somesecret&algorithm=SHA1&digits=6&period=30"
+    );
+
+    // Valid original with no params, edit with secret only
+    // => sanitize secret and add default params
+    assert_eq!(
+        uri_for_saving("otpauth://totp/?secret=original_secret", "new secret"),
+        "otpauth://totp/?secret=newsecret&algorithm=SHA1&digits=6&period=30"
+    );
+
+    // Valid original with issuer and no params, edit with secret only
+    // => sanitize secret, use the original issuer and add default params
+    assert_eq!(
+        uri_for_saving(
+            "otpauth://totp/?secret=original_secret&issuer=original_issuer",
+            "new secret"
+        ),
+        "otpauth://totp/?secret=newsecret&issuer=original_issuer&algorithm=SHA1&digits=6&period=30"
+    );
+
+    // Valid original with default params, edit with secret only
+    // => sanitize secret and add default params
+    assert_eq!(
+        uri_for_saving(
+            "otpauth://totp/?secret=original_secret&algorithm=SHA1&digits=6&period=30",
+            "new secret"
+        ),
+        "otpauth://totp/?secret=newsecret&algorithm=SHA1&digits=6&period=30"
+    );
+
+    // Valid original with custom params, edit with secret only
+    // => sanitize secret and add default params
+    assert_eq!(
+        uri_for_saving(
+            "otpauth://totp/?secret=original_secret&algorithm=SHA256&digits=8&period=45",
+            "new secret"
+        ),
+        "otpauth://totp/?secret=newsecret&algorithm=SHA1&digits=6&period=30"
+    );
+
+    // Valid original with custom params, edit with custom params and issuer
+    // => save the edited URI as-is
+    assert_eq!(
+        uri_for_saving(
+            "otpauth://totp/?secret=original_secret&algorithm=SHA256&digits=8&period=45",
+            "otpauth://totp/?secret=new_secret&issuer=new_issuer&algorithm=SHA1&digits=8&period=45"
+        ),
+        "otpauth://totp/?secret=new_secret&issuer=new_issuer&algorithm=SHA1&digits=8&period=45"
+    );
 }

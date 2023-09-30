01d8c8ff48f143b7eb42d4e454bb57cfecc94316
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-09-30 13:58:17 +0200

    feat: expose sanitizer and parser via common.udl

--- proton-pass-mobile/src/totp.rs
+++ proton-pass-mobile/src/totp.rs
@@ -1,15 +1,32 @@
+pub use proton_pass_common::totp::components::TOTPComponents;
+pub use proton_pass_common::totp::error::TOTPError;
+use proton_pass_common::totp::sanitizer::{uri_for_editing, uri_for_saving};
+
+pub struct TotpUriParser;
 pub struct TotpUriSanitizer;
+
+pub type TOTPAlgorithm = proton_pass_common::totp::algorithm::Algorithm;
+
+impl TotpUriParser {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn parse(&self, uri_string: String) -> Result<TOTPComponents, TOTPError> {
+        TOTPComponents::from_uri(uri_string.as_str())
+    }
+}
 
 impl TotpUriSanitizer {
     pub fn new() -> Self {
         Self
     }
 
-    pub fn uri_for_editing(&self, original_uri: &str) -> String {
-        proton_pass_common::totp::sanitizer::uri_for_editing(original_uri)
+    pub fn uri_for_editing(&self, original_uri: String) -> String {
+        uri_for_editing(original_uri.as_str())
     }
 
-    pub fn uri_for_saving(&self, original_uri: &str, edited_uri: &str) -> String {
-        proton_pass_common::totp::sanitizer::uri_for_saving(original_uri, edited_uri)
+    pub fn uri_for_saving(&self, original_uri: String, edited_uri: String) -> String {
+        uri_for_saving(original_uri.as_str(), edited_uri.as_str())
     }
 }

fdf6e5a69b5c3accd2ce90a073e5c54ade8bc4d7
Victor Hidalgo <victor.hidalgo@proton.ch>
2023-10-27 11:30:15 +0200

    (cargo-release) Bump version

--- proton-pass-mobile/android/lib/build.gradle.kts
+++ proton-pass-mobile/android/lib/build.gradle.kts
@@ -61,7 +61,7 @@ android {
 
 mavenPublishing {
     group = "me.proton.pass.common"
-    version = "0.3.0"
+    version = "0.3.1"
     pom {
         scm {
             connection.set(gitHubDomain)

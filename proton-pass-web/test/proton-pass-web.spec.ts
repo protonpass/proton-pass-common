6dc90adea23ed602389ae2dc5533afa56ddb6add
Edvin CANDON <edvin.candon@proton.ch>
2023-12-15 14:59:17 +0100

    Fix bun tests

---
+++ proton-pass-web/test/proton-pass-web.spec.ts
@@ -0,0 +1,32 @@
+import { describe, expect, test } from "bun:test";
+import * as packageJSON from "../pkg/package.json";
+
+import {
+  detect_credit_card_type,
+  generate_passphrase,
+  library_version,
+  random_words,
+} from "../pkg/proton_pass_web.mjs";
+
+describe("ProtonPassWeb WASM", () => {
+  test("Library version", () => {
+    expect(library_version()).toEqual(packageJSON.version);
+  });
+
+  test("Should create passphrase", () => {
+    const words = random_words(3);
+    const passphrase = generate_passphrase(words, {
+      separator: "Hyphens",
+      capitalise: true,
+      include_numbers: true,
+      count: 3,
+    });
+
+    expect(passphrase).not.toBeUndefined();
+  });
+
+  test("Should detect CC", () => {
+    const cardType = detect_credit_card_type("4000056655665556");
+    expect(cardType).toEqual("Visa");
+  });
+});

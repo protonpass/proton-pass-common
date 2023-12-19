0e4174e49df5891bd56969a586f9e7d980ee094e
Carlos Quintana <carlos.quintana@proton.ch>
2023-12-19 13:38:08 +0100

    feat: add generated passwords list and buildscript integration

--- proton-pass-common/build.rs
+++ proton-pass-common/build.rs
@@ -5,13 +5,45 @@ use std::path::Path;
 
 // https://doc.rust-lang.org/cargo/reference/build-scripts.html#case-study-code-generation
 fn main() {
+    build_eff_wordlist();
+    build_common_password_list();
+}
+
+fn build_eff_wordlist() {
     let out_dir = env::var("OUT_DIR").expect("OUT_DIR not set");
     let dest_path = Path::new(&out_dir).join("wordlists.rs");
     let f = File::create(dest_path).expect("Could not create wordlists.rs");
-    words(&f, "EFF_LARGE_WORDLIST", "eff_large_wordlist.txt");
+    eff_wordlist(&f, "EFF_LARGE_WORDLIST", "eff_large_wordlist.txt");
 }
 
-fn words(mut f_dest: &File, const_name: &str, fname_src: &str) {
+fn build_common_password_list() {
+    let out_dir = env::var("OUT_DIR").expect("OUT_DIR not set");
+    let dest_path = Path::new(&out_dir).join("common_passwords.rs");
+    let f = File::create(dest_path).expect("Could not create common_passwords.rs");
+    common_passwords(&f, "COMMON_PASSWORDS", "passwords.txt");
+}
+
+fn common_passwords(mut dst: &File, const_name: &str, filename: &str) {
+    dst.write_all(b"const ").expect("Error writing common password list");
+    dst.write_all(const_name.as_bytes())
+        .expect("Error writing common password list");
+    dst.write_all(b": &[&str] = &[")
+        .expect("Error writing common password list");
+
+    let src = BufReader::new(File::open(filename).unwrap_or_else(|e| panic!("Error opening {filename}: {e}")));
+
+    for line in src.lines() {
+        let password = line.expect("Error reading line from file");
+        dst.write_all(b"\"").expect("Error writing common password");
+        dst.write_all(password.as_bytes())
+            .expect("Error writing common password");
+        dst.write_all(b"\",").expect("Error writing common password");
+    }
+
+    dst.write_all(b"];").expect("Error writing wordlist");
+}
+
+fn eff_wordlist(mut f_dest: &File, const_name: &str, fname_src: &str) {
     f_dest.write_all(b"const ").expect("Error writing wordlist");
     f_dest.write_all(const_name.as_bytes()).expect("Error writing wordlist");
     f_dest.write_all(b": &[&str] = &[").expect("Error writing wordlist");

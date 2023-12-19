df6f15a4fc02c4d2f3016c3039fdf8c3eba38d45
Carlos Quintana <carlos.quintana@proton.ch>
2023-12-19 14:56:49 +0100

    chore: add benchmark to password scorer

---
+++ proton-pass-common/benches/password_scorer.rs
@@ -0,0 +1,12 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use proton_pass_common::password::check_score;
+
+fn password_scorer(c: &mut Criterion) {
+    c.bench_function("score weak password", |b| b.iter(|| black_box(check_score("qwerty"))));
+    c.bench_function("score strong password", |b| {
+        b.iter(|| black_box(check_score("o4L7^_*[Ai!9Hf4-_5g^T")))
+    });
+}
+
+criterion_group!(benches, password_scorer);
+criterion_main!(benches);

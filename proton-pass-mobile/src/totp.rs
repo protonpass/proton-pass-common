6f87184a64ff736af5e4e75a66133018c0b32975
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-01-06 09:20:07 +0100

    feat: add and expose unwrap functions

--- proton-pass-mobile/src/totp.rs
+++ proton-pass-mobile/src/totp.rs
@@ -3,20 +3,29 @@ use proton_pass_common::totp::sanitizer::{uri_for_editing, uri_for_saving};
 pub use proton_pass_common::totp::totp::TotpTokenResult;
 pub use proton_pass_common::totp::totp::TOTP;
 
-pub struct TotpUriParser;
-pub struct TotpUriSanitizer;
-
 pub type TOTPAlgorithm = proton_pass_common::totp::algorithm::Algorithm;
 
-impl TotpUriParser {
+pub struct TotpHandler;
+
+impl TotpHandler {
     pub fn new() -> Self {
         Self
     }
 
-    pub fn parse(&self, uri_string: String) -> Result<TOTP, TOTPError> {
-        TOTP::from_uri(uri_string.as_str())
+    pub fn unwrap_algorithm(&self, totp: TOTP) -> TOTPAlgorithm {
+        totp.unwrap_algorithm()
+    }
+
+    pub fn unwrap_digits(&self, totp: TOTP) -> u8 {
+        totp.unwrap_digits()
+    }
+
+    pub fn unwrap_period(&self, totp: TOTP) -> u16 {
+        totp.unwrap_period()
     }
 }
+
+pub struct TotpUriSanitizer;
 
 impl TotpUriSanitizer {
     pub fn new() -> Self {

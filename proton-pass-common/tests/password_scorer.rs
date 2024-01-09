5aae3521f319d43674e10d051d10900161c831ae
Carlos Quintana <carlos.quintana@proton.ch>
2024-01-09 14:48:09 +0100

    test(common): add more tests to password scorer

--- proton-pass-common/tests/password_scorer.rs
+++ proton-pass-common/tests/password_scorer.rs
@@ -17,11 +17,33 @@ macro_rules! map {
 #[test]
 fn score_passwords() {
     let cases = map!(
+        // Short passwords
         "" => PasswordScore::Vulnerable,
         "a" => PasswordScore::Vulnerable,
         "abcde" => PasswordScore::Vulnerable,
-        "abcABC123" => PasswordScore::Vulnerable,
-        "azK@BC123" => PasswordScore::Vulnerable,
+
+        "abcABC123" => PasswordScore::Vulnerable, // Lowercase, Uppercase and numbers (9)
+        "abcABC123pqj" => PasswordScore::Vulnerable, // Lowercase, Uppercase and numbers (12)
+
+        "azK@BC123" => PasswordScore::Vulnerable, // Lowercase, Uppercase, numbers and symbol (9)
+        "azK@BC123pqj" => PasswordScore::Weak, // Lowercase, Uppercase, numbers and symbol (12)
+
+        "apjq4n9b2kb2jhgj" => PasswordScore::Vulnerable, // only lowercase and numbers and mildly long (16)
+        "1847382519758729" => PasswordScore::Vulnerable, // only numbers and mildly long (16)
+        "apqkfjwuiwjkersg" => PasswordScore::Vulnerable, // only lowercase and mildly long (16)
+        "EFGUSHWEFUIAJKBE" => PasswordScore::Vulnerable, // only uppercase and mildly long (16)
+
+        "apJEhqCIkeVJpUhA" => PasswordScore::Weak, // only lowercase and uppercase and mildly long (16)
+        "apJEhqCIkeVJpUhAr" => PasswordScore::Strong, // only lowercase and uppercase (17)
+
+        "apjq4n9b2kb2jhgjo1nd" => PasswordScore::Strong, // only lowercase and numbers but very long (20)
+        "34976128647294268053" => PasswordScore::Strong, // only numbers but very long (20)
+        "apqkfjwuiwjkersgyuih" => PasswordScore::Strong, // only lowercase but very long (20)
+        "EFGUSHWEFUIAJKBERNJS" => PasswordScore::Strong, // only uppercase but very long (20)
+
+        "_:^¿" => PasswordScore::Vulnerable, // only symbols - short(5)
+        "_:^¿=($#-@+/" => PasswordScore::Weak, // only symbols - medium(12)
+        "_:^¿=($#-@+/*-%)$!?-" => PasswordScore::Strong, // only symbols but long (20)
 
         // Appears in a common password list
         "Qwerty12345678" => PasswordScore::Vulnerable,
@@ -30,10 +52,11 @@ fn score_passwords() {
 
     for (input, expected) in cases {
         let score = check_score(input);
+        let password_score = score.password_score;
         assert_eq!(
-            score, expected,
+            password_score, expected,
             "{} expected to be {:?} but was {:?}",
-            input, expected, score
+            input, expected, password_score
         );
     }
 }

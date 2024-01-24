740b057233cad202b98c778d479bfd9b13051702
Carlos Quintana <carlos.quintana@proton.ch>
2024-01-24 15:24:33 +0100

    add passkey support to mobile

---
+++ proton-pass-mobile/src/passkey.rs
@@ -0,0 +1,50 @@
+use proton_pass_common::passkey::{generate_passkey_for_domain, resolve_challenge_for_domain, PasskeyResult};
+pub use proton_pass_common::passkey::PasskeyError;
+
+pub struct CreatePasskeyResponse {
+    pub passkey: Vec<u8>,
+    pub response: String,
+}
+
+pub struct PasskeyManager {
+    rt: tokio::runtime::Runtime,
+}
+
+impl PasskeyManager {
+    pub fn new() -> PasskeyResult<Self> {
+        match tokio::runtime::Builder::new_current_thread().build() {
+            Ok(rt) => Ok(Self { rt }),
+            Err(e) => Err(PasskeyError::RuntimeError(format!("Error creating runtime: {:?}", e))),
+        }
+    }
+
+    pub fn generate_passkey(
+        &self,
+        url: String,
+        display_name: String,
+        challenge_bytes: Vec<u8>,
+    ) -> PasskeyResult<CreatePasskeyResponse> {
+        self.rt
+            .handle()
+            .block_on(async move {
+                match generate_passkey_for_domain(&url, &display_name, challenge_bytes).await {
+                    Ok(r) => match r.response() {
+                        Ok(response) => Ok(CreatePasskeyResponse {
+                            passkey: r.passkey,
+                            response,
+                        }),
+                        Err(e) => Err(e)
+                    },
+                    Err(e) => Err(e)
+                }
+            })
+    }
+
+    pub fn resolve_challenge(&self, url: String, passkey: Vec<u8>, challenge_bytes: Vec<u8>) -> PasskeyResult<()> {
+        self.rt.handle().block_on(async move {
+            resolve_challenge_for_domain(&url, &passkey, challenge_bytes)
+                .await
+                .map(|_| ())
+        })
+    }
+}

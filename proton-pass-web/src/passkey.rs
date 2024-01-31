eec931b7fc32e96d4a8ed2cc2f5e8cfd707f46b9
Carlos Quintana <carlos.quintana@proton.ch>
2024-01-31 14:36:14 +0100

    chore: expose passkey functions on web

--- proton-pass-web/src/passkey.rs
+++ proton-pass-web/src/passkey.rs
@@ -1,10 +1,25 @@
-use proton_pass_common::passkey::{
-    generate_passkey_for_domain, resolve_challenge_for_domain, CreatePassKeyResponse, PasskeyError, PasskeyResult,
-    ResolveChallengeResponse,
-};
+use proton_pass_common::passkey::{generate_passkey_for_domain, resolve_challenge_for_domain, PasskeyError, PasskeyResult};
+use serde::{Deserialize, Serialize};
+use tsify::Tsify;
 
 pub struct PasskeyManager {
     rt: tokio::runtime::Runtime,
+}
+
+#[derive(Tsify, Deserialize, Serialize)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub struct WasmGeneratePasskeyResponse {
+    pub passkey: Vec<u8>,
+    pub response: String,
+    pub rp_name: String,
+    pub user_name: String,
+    pub user_display_name: String,
+}
+
+#[derive(Tsify, Deserialize, Serialize)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub struct WasmResolvePasskeyChallengeResponse {
+    pub response: String,
 }
 
 impl PasskeyManager {
@@ -15,10 +30,20 @@ impl PasskeyManager {
         }
     }
 
-    pub fn generate_passkey(&self, url: String, request: String) -> PasskeyResult<CreatePassKeyResponse> {
-        self.rt
+    pub fn generate_passkey(&self, url: String, request: String) -> PasskeyResult<WasmGeneratePasskeyResponse> {
+        let res = self.rt
             .handle()
-            .block_on(async move { generate_passkey_for_domain(&url, &request).await })
+            .block_on(async move { generate_passkey_for_domain(&url, &request).await })?;
+
+        let response = res.response()?;
+
+        Ok(WasmGeneratePasskeyResponse {
+            passkey: res.passkey,
+            response,
+            rp_name: res.rp_name,
+            user_name: res.user_name,
+            user_display_name: res.user_display_name,
+        })
     }
 
     pub fn resolve_challenge(
@@ -26,9 +51,13 @@ impl PasskeyManager {
         url: String,
         passkey: Vec<u8>,
         request: String,
-    ) -> PasskeyResult<ResolveChallengeResponse> {
-        self.rt
+    ) -> PasskeyResult<WasmResolvePasskeyChallengeResponse> {
+        let res = self.rt
             .handle()
-            .block_on(async move { resolve_challenge_for_domain(&url, &passkey, &request).await })
+            .block_on(async move { resolve_challenge_for_domain(&url, &passkey, &request).await })?;
+
+        let response = res.response()?;
+
+        Ok(WasmResolvePasskeyChallengeResponse { response })
     }
 }

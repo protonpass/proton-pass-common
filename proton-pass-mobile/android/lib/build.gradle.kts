fdfc0c315fc5355733ea5ad6f732e1e74eafcf52
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-02-26 11:55:12 +0100

    (cargo-release) Bump version

--- proton-pass-mobile/android/lib/build.gradle.kts
+++ proton-pass-mobile/android/lib/build.gradle.kts
@@ -61,7 +61,7 @@ android {
 
 mavenPublishing {
     group = "me.proton.pass.common"
-    version = "0.6.6"
+    version = "0.6.7"
     pom {
         scm {
             connection.set(gitHubDomain)

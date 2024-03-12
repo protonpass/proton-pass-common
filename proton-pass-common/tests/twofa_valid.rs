13a546d3dc2f44d09466fdda9935ce60687f1712
Martin Lukacs <martin.lukacs@proton.ch>
2024-03-12 14:31:42 +0100

    fix: tests

--- proton-pass-common/tests/twofa_valid.rs
+++ proton-pass-common/tests/twofa_valid.rs
@@ -1,17 +1,19 @@
 use proton_pass_common::twofa::TwofaDomainCheck;
 
 #[test]
-fn domain_exist_in_set() {
-    let manager = TwofaDomainCheck::new().expect("Could not create domain checker");
+fn domain_exist_in_set() { 
+    let manager = TwofaDomainCheck::new()
+    .expect("Could not create domain checker");
     let domain = "34SP.com";
 
-    assert!(manager.check_existence(domain))
+    assert!(manager.twofa_domain_eligible(domain))
 }
 
 #[test]
-fn domain_doesnt_exist_in_set() {
-    let manager = TwofaDomainCheck::new().expect("Could not create domain checker");
-    let domain = "zoggyzzog";
+fn domain_doesnt_exist_in_set() { 
+    let manager = TwofaDomainCheck::new()
+    .expect("Could not create domain checker");
+    let domain = "testNonExistingDomain.com";
 
-    assert!(!manager.check_existence(domain))
+    assert!(!manager.twofa_domain_eligible(domain))
 }

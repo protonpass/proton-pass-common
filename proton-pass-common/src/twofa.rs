f8b630d7e5a00431b4ee3b4c058c5aad1a6f9822
Carlos Quintana <carlos.quintana@proton.ch>
2024-03-12 16:30:57 +0100

    chore: only convert 2fa domain list to set once

--- proton-pass-common/src/twofa.rs
+++ proton-pass-common/src/twofa.rs
@@ -1,22 +1,22 @@
 use std::collections::HashSet;
 use std::env;
 
-pub struct TwofaDomainChecker {
-    set: HashSet<String>,
-}
-
 include!(concat!(env!("OUT_DIR"), "/twofa_domains.rs"));
 
-impl Default for TwofaDomainChecker {
-    fn default() -> Self {
-        Self {
-            set: TWOFA_DOMAINS.iter().map(|&s| s.to_string()).collect(),
-        }
-    }
+lazy_static::lazy_static! {
+    static ref DOMAINS: HashSet<String> = {
+        let mut set = HashSet::new();
+        TWOFA_DOMAINS.iter().for_each(|s| {
+            set.insert(s.to_string());
+        });
+        set
+    };
 }
 
+pub struct TwofaDomainChecker;
+
 impl TwofaDomainChecker {
-    pub fn twofa_domain_eligible(&self, term: &str) -> bool {
-        self.set.contains(term)
+    pub fn twofa_domain_eligible(term: &str) -> bool {
+        DOMAINS.contains(term)
     }
 }

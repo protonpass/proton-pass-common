2d8d4dcc0a954ba27ff4974792ce032419c5b501
Martin Lukacs <martin.lukacs@proton.ch>
2024-03-12 13:56:03 +0100

    fix: Correction of compilation bugs

--- proton-pass-common/src/twofa.rs
+++ proton-pass-common/src/twofa.rs
@@ -1,25 +1,25 @@
 use std::collections::HashSet;
+use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
-use std::env;
 
 pub struct TwofaDomainCheck {
     set: HashSet<String>,
 }
 
 impl TwofaDomainCheck {
-       const DEFAULT_FILE_NAME: &'static str = "2faDomains.txt";
+    const DEFAULT_FILE_NAME: &'static str = "2faDomains.txt";
 
-       pub fn new() -> io::Result<Self> {
-           let mut default_path = env::current_dir()?;
-           default_path.push(Self::DEFAULT_FILE_NAME);
-   
-           let file = File::open(default_path)?;
-           let lines = BufReader::new(file).lines();
-            let set: HashSet<String> = lines.flatten().collect();
+    pub fn new() -> io::Result<Self> {
+        let mut default_path = env::current_dir()?;
+        default_path.push(Self::DEFAULT_FILE_NAME);
 
-           Ok(TwofaDomainCheck { set })
-       }
+        let file = File::open(default_path)?;
+        let lines = BufReader::new(file).lines();
+        let set: HashSet<String> = lines.map_while(Result::ok).collect();
+
+        Ok(TwofaDomainCheck { set })
+    }
 
     pub fn twofa_domain_eligible(&self, term: &str) -> bool {
         self.set.contains(term)

8f47db24453e55fff14b79c9be79b2628dca8332
Martin Lukacs <martin.lukacs@proton.ch>
2024-03-12 11:43:24 +0100

    (cargo-release) Bump version

--- proton-pass-mobile/android/lib/build.gradle.kts
+++ proton-pass-mobile/android/lib/build.gradle.kts
@@ -61,7 +61,7 @@ android {
 
 mavenPublishing {
     group = "me.proton.pass.common"
-    version = "0.6.10"
+    version = "0.6.11"
     pom {
         scm {
             connection.set(gitHubDomain)

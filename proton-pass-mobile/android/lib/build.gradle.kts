ca2fa2c054413acb048d6eec7d922bfa9012252d
Edvin CANDON <edvin.candon@proton.ch>
2024-05-17 10:59:29 +0200

    (cargo-release) Bump version

--- proton-pass-mobile/android/lib/build.gradle.kts
+++ proton-pass-mobile/android/lib/build.gradle.kts
@@ -61,7 +61,7 @@ android {
 
 mavenPublishing {
     group = "me.proton.pass.common"
-    version = "0.7.11"
+    version = "0.7.12"
     pom {
         scm {
             connection.set(gitHubDomain)

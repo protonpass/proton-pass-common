c1cfc08f797de5b2f1af37eb3e2fe4c15c3f31cf
Carlos Quintana <carlos.quintana@proton.ch>
2024-05-29 09:50:29 +0200

    (cargo-release) Bump version

--- proton-pass-mobile/android/lib/build.gradle.kts
+++ proton-pass-mobile/android/lib/build.gradle.kts
@@ -61,7 +61,7 @@ android {
 
 mavenPublishing {
     group = "me.proton.pass.common"
-    version = "0.7.12"
+    version = "0.7.13"
     pom {
         scm {
             connection.set(gitHubDomain)

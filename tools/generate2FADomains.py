5f9f49ee0038d44dfa731a87c63027b730e0278b
Dorian Turba <commit.9wuzh@slmail.me>
2024-06-06 17:41:22 +0200

    refactor: less nesting and early exit

--- tools/generate2FADomains.py
+++ tools/generate2FADomains.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import http
 import json
 import pathlib
 import urllib.request
@@ -24,36 +24,36 @@ excluded_domains = [
     if (domain_ := domain.strip())
 ]
 
-if response.status == 200:
-    # Load JSON data from the response
-
-    body = response.read()
-    text = body.decode("utf-8")
-
-    data = json.loads(text)
-
-    # Extract domains
-    domains = []
-    for item in data:
-        if isinstance(item, list) and len(item) > 1 and "domain" in item[1]:
-            domain = item[1]["domain"]
-            if domain not in excluded_domains:
-                domains.append(domain)
-
-    # Open the custom domains file in read mode
-    with open(CUSTOM_DOMAINS_FILE) as file:
-        for line in file.readlines():
-            custom_domain = line.strip()
-            if custom_domain not in excluded_domains:
-                domains.append(custom_domain)
-
-    clean_domains = sorted(list(set(domains)))
-
-    # Save domains to a text file
-    with open(DEFAULT_DESTINATION, "w") as file:
-        for domain in clean_domains:
-            file.write(domain + "\n")
-
-    print("Domains have been extracted and saved to domains.txt")
-else:
+if response.status != http.HTTPStatus.OK:
     print(f"Failed to download the JSON data. Status code: {response.status_code}")
+    exit(1)
+
+# Load JSON data from the response
+body = response.read()
+text = body.decode("utf-8")
+
+data = json.loads(text)
+
+# Extract domains
+domains = []
+for item in data:
+    if isinstance(item, list) and len(item) > 1 and "domain" in item[1]:
+        domain = item[1]["domain"]
+        if domain not in excluded_domains:
+            domains.append(domain)
+
+# Open the custom domains file in read mode
+with open(CUSTOM_DOMAINS_FILE) as file:
+    for line in file.readlines():
+        custom_domain = line.strip()
+        if custom_domain not in excluded_domains:
+            domains.append(custom_domain)
+
+clean_domains = sorted(list(set(domains)))
+
+# Save domains to a text file
+with open(DEFAULT_DESTINATION, "w") as file:
+    for domain in clean_domains:
+        file.write(domain + "\n")
+
+print("Domains have been extracted and saved to domains.txt")

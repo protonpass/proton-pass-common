30d3d7bf5826a5985c960e23515c48ee5f28f8f1
Dorian Turba <commit.9wuzh@slmail.me>
2024-06-06 20:16:42 +0200

    refactor: organize in functions

--- tools/generate2FADomains.py
+++ tools/generate2FADomains.py
@@ -2,6 +2,7 @@
 import http.client
 import json
 import pathlib
+import typing
 import urllib.request
 
 # https://2fa.directory/api/
@@ -12,29 +13,45 @@ DEFAULT_DESTINATION = (
     pathlib.Path(__file__).parent.parent / "proton-pass-common" / "2faDomains.txt"
 )
 
-request = urllib.request.Request(
-    URL, headers={"Accept": "application/json", "User-Agent": "curl/7.81.0"}
-)
-response: http.client.HTTPResponse = urllib.request.urlopen(request)
 
-excluded_domains = {
-    domain.strip() for domain in EXCLUDE_DOMAINS_FILE.read_text().split("\n")
-}
-excluded_domains -= {""}  # Remove empty lines
+class Metadata(typing.TypedDict):
+    domain: str
 
-if response.status != http.HTTPStatus.OK:
-    print(f"Failed to download the JSON data. Status code: {response.status}")
-    exit(1)
 
-body = response.read()
-text = body.decode("utf-8")
-data = json.loads(text)
+def get_2fa_data() -> typing.List[typing.Tuple[str, Metadata]]:
+    request = urllib.request.Request(
+        URL, headers={"Accept": "application/json", "User-Agent": "curl/7.81.0"}
+    )
+    response: http.client.HTTPResponse = urllib.request.urlopen(request)
+    if response.status != http.HTTPStatus.OK:
+        print(f"Failed to download the JSON data. Status code: {response.status}")
+        exit(1)
+    body = response.read()
+    text = body.decode("utf-8")
+    data = json.loads(text)
+    return data
 
-domains = {metadata["domain"] for _, metadata in data}
-domains |= {domain.strip() for domain in CUSTOM_DOMAINS_FILE.read_text().split("\n")}
-domains -= excluded_domains
-domains = sorted(domains)
 
-DEFAULT_DESTINATION.write_text("\n".join(domains))
+def generate_domains(data: typing.List[typing.Tuple[str, Metadata]]):
+    domains = {metadata["domain"] for _, metadata in data}
+    domains |= {
+        domain.strip() for domain in CUSTOM_DOMAINS_FILE.read_text().split("\n")
+    }
+    excluded_domains = {
+        domain.strip() for domain in EXCLUDE_DOMAINS_FILE.read_text().split("\n")
+    }
+    excluded_domains -= {""}  # Remove empty lines
+    domains -= excluded_domains
+    domains = sorted(domains)
+    return domains
 
-print("Domains have been extracted and saved to domains.txt")
+
+def generate_domains_file() -> None:
+    data = get_2fa_data()
+    domains = generate_domains(data)
+    DEFAULT_DESTINATION.write_text("\n".join(domains))
+    print("Domains have been extracted and saved to domains.txt")
+
+
+if __name__ == "__main__":
+    generate_domains_file()

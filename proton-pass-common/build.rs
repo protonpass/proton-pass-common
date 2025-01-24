b15a36aa273ee56edf81756974670be526ccb9ec
Carlos Quintana <carlos.quintana@proton.ch>
2025-01-24 16:16:56 +0100

    build: add rerun-if-changed directives to build.rs

--- proton-pass-common/build.rs
+++ proton-pass-common/build.rs
@@ -12,6 +12,7 @@ fn main() {
 }
 
 fn build_eff_wordlist() {
+    println!("cargo:rerun-if-changed=eff_words.txt");
     let out_dir = env::var("OUT_DIR").expect("OUT_DIR not set");
     let dest_path = Path::new(&out_dir).join("wordlists.rs");
     let f = File::create(dest_path).expect("Could not create wordlists.rs");
@@ -19,6 +20,7 @@ fn build_eff_wordlist() {
 }
 
 fn build_common_password_list() {
+    println!("cargo:rerun-if-changed=passwords.txt");
     let out_dir = env::var("OUT_DIR").expect("OUT_DIR not set");
     let dest_path = Path::new(&out_dir).join("common_passwords.rs");
     let f = File::create(dest_path).expect("Could not create common_passwords.rs");
@@ -26,6 +28,7 @@ fn build_common_password_list() {
 }
 
 fn build_2fa_domains_list() {
+    println!("cargo:rerun-if-changed=2faDomains.txt");
     let out_dir = env::var("OUT_DIR").expect("OUT_DIR not set");
     let dest_path = Path::new(&out_dir).join("twofa_domains.rs");
     let f = File::create(dest_path).expect("Could not create twofaDomains.rs");
@@ -94,6 +97,7 @@ fn eff_wordlist(mut f_dest: &File, const_name: &str, fname_src: &str) {
 }
 
 fn generate_google_authenticator_proto() {
+    println!("cargo:rerun-if-changed=proto/google_authenticator.proto");
     let proto_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("proto");
     let proto_path = proto_dir.join("google_authenticator.proto");
     let out_dir = Path::new(env!("CARGO_MANIFEST_DIR"))

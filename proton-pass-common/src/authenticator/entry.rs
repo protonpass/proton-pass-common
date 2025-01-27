4020cfe5cef57a49e24eb7f34949a6be4d7366dd
Carlos Quintana <carlos.quintana@proton.ch>
2025-01-27 17:00:23 +0100

    chore: add aegis txt import

--- proton-pass-common/src/authenticator/entry.rs
+++ proton-pass-common/src/authenticator/entry.rs
@@ -23,9 +23,9 @@ impl AuthenticatorEntry {
         let parsed = url::Url::parse(uri).map_err(|_| AuthenticatorEntryError::UnsupportedUri)?;
         let content = match parsed.scheme() {
             "otpauth" => {
-                if parsed.scheme() == "steam" {
-                    let steam_parsed = SteamTotp::new_from_parsed_uri(&parsed, false)
-                        .map_err(|_| AuthenticatorEntryError::ParseError)?;
+                if parsed.host_str() == Some("steam") {
+                    let steam_parsed =
+                        SteamTotp::new_from_otp_uri(&parsed).map_err(|_| AuthenticatorEntryError::ParseError)?;
                     AuthenticatorEntryContent::Steam(steam_parsed)
                 } else {
                     let totp = TOTP::from_uri(uri).map_err(|_| AuthenticatorEntryError::ParseError)?;

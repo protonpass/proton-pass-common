28e95c897d8d01241cc5cf83ba848832779ed3db
Carlos Quintana <carlos.quintana@proton.ch>
2025-02-07 08:30:52 +0100

    chore(authenticator-mobile): add missing Package.swift file

---
+++ proton-authenticator-mobile/iOS/AuthenticatorRustCore/Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AuthenticatorRustCore",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AuthenticatorRustCore",
+            targets: ["AuthenticatorRustCore"]),
+    ],
+    targets: [
+        .binaryTarget(name: "RustFrameworkFFI", path: "./RustFramework.xcframework"),
+
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AuthenticatorRustCore",
+            dependencies: [
+                .target(name: "RustFrameworkFFI")
+            ],
+            swiftSettings: [
+                .unsafeFlags(["-suppress-warnings"]),
+            ]
+        )
+    ]
+)

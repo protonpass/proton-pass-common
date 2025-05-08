85bc82bcfe77c658f04c3c6829df451a09047078
Carlos Quintana <carlos.quintana@proton.ch>
2025-05-08 12:17:44 +0200

    fix(authenticator): lastpass importer omit unused fields

--- proton-authenticator/src/parser/lastpass/json.rs
+++ proton-authenticator/src/parser/lastpass/json.rs
@@ -7,10 +7,6 @@ use proton_pass_totp::totp::TOTP;
 
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
 struct Account {
-    #[serde(rename = "accountID")]
-    pub account_id: String,
-    #[serde(rename = "lmiUserId")]
-    pub lmi_user_id: String,
     #[serde(rename = "issuerName")]
     pub issuer_name: String,
     #[serde(rename = "originalIssuerName")]
@@ -19,29 +15,17 @@ struct Account {
     pub user_name: String,
     #[serde(rename = "originalUserName")]
     pub original_user_name: String,
-    #[serde(rename = "pushNotification")]
-    pub push_notification: bool,
     pub secret: String,
     #[serde(rename = "timeStep")]
     pub time_step: u16,
     pub digits: u8,
     #[serde(rename = "creationTimestamp")]
     pub creation_timestamp: u64,
-    #[serde(rename = "isFavorite")]
-    pub is_favorite: bool,
     pub algorithm: String,
 }
 
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
 struct Root {
-    #[serde(rename = "deviceId")]
-    pub device_id: String,
-    #[serde(rename = "deviceSecret")]
-    pub device_secret: String,
-    #[serde(rename = "localDeviceId")]
-    pub local_device_id: String,
-    #[serde(rename = "deviceName")]
-    pub device_name: String,
     pub version: u16,
     pub accounts: Vec<Account>,
 }
@@ -156,6 +140,45 @@ mod test {
             AuthenticatorEntryContent::Totp(totp) => {
                 check_totp(totp, Algorithm::SHA512, 6, 30, None, Some("sha512 name".to_string()));
             }
+            _ => panic!("Should be a TOTP"),
+        }
+    }
+
+    #[test]
+    fn can_parse_lastpass_ios_export() {
+        let input = get_file_contents("lastpass/lastpass_ios_export.json");
+
+        let res = parse_lastpass_json(&input).expect("should be able to parse");
+        assert!(res.errors.is_empty());
+
+        let entries = res.entries;
+        assert_eq!(entries.len(), 2);
+
+        match &entries[0].content {
+            AuthenticatorEntryContent::Totp(totp) => {
+                check_totp(
+                    totp,
+                    Algorithm::SHA1,
+                    6,
+                    30,
+                    Some("code 1 lastpass".to_string()),
+                    Some("snjxxndjbdbdb".to_string()),
+                )
+            },
+            _ => panic!("Should be a TOTP"),
+        }
+
+        match &entries[1].content {
+            AuthenticatorEntryContent::Totp(totp) => {
+                check_totp(
+                    totp,
+                    Algorithm::SHA1,
+                    6,
+                    30,
+                    Some("code2 lastpass".to_string()),
+                    Some("ndjdbxxnbcb".to_string()),
+                )
+            },
             _ => panic!("Should be a TOTP"),
         }
     }

897846458d4e1c3e10b9e5628b5752ee1957a967
Carlos Quintana <carlos.quintana@proton.ch>
2025-06-26 10:37:59 +0200

    (cargo-release) Bump version

--- proton-authenticator-mobile/android/lib/build.gradle.kts
+++ proton-authenticator-mobile/android/lib/build.gradle.kts
@@ -60,7 +60,7 @@ android {
 
 mavenPublishing {
     group = "me.proton.authenticator.common"
-    version = "0.22.0"
+    version = "0.22.1"
     pom {
         scm {
             connection.set(gitHubDomain)

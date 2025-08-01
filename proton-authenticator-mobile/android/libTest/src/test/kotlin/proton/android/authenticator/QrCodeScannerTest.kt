8927b1f5e9f704280048a9b7ea9764490d2f1762
Carlos Quintana <carlos.quintana@proton.ch>
2025-08-01 14:57:33 +0200

    test(authenticator-mobile): unit tests for qr code scanner

---
+++ proton-authenticator-mobile/android/libTest/src/test/kotlin/proton/android/authenticator/QrCodeScannerTest.kt
@@ -0,0 +1,50 @@
+package proton.android.authenticator
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import uniffi.proton_authenticator_common_mobile.QrCodeScanner
+import java.io.File
+
+class QrCodeScannerTest {
+
+    private val scanner = QrCodeScanner()
+
+    @Test
+    fun `can import example QR code`() {
+        val contents = importQrCode("example.png")
+
+        val content = scanner.scanQrCode(contents)
+        assertThat(content).isEqualTo("This is an example")
+    }
+
+    @Test
+    fun `empty content returns null`() {
+        val content = scanner.scanQrCode(byteArrayOf())
+        assertThat(content).isNull()
+    }
+
+    @Test
+    fun `can import google authenticator export QR code 1`() {
+        canImportGoogleAuthenticatorQrCode("GoogleAuthenticatorExport_1.png")
+    }
+
+    @Test
+    fun `can import google authenticator export QR code 2`() {
+        canImportGoogleAuthenticatorQrCode("GoogleAuthenticatorExport_2.png")
+    }
+
+    @Test
+    fun `can import google authenticator export QR code 3`() {
+        canImportGoogleAuthenticatorQrCode("GoogleAuthenticatorExport_3.png")
+    }
+
+    private fun canImportGoogleAuthenticatorQrCode(filename: String) {
+        val contents = importQrCode(filename)
+
+        val content = scanner.scanQrCode(contents)
+        assertThat(content).contains("otpauth-migration://")
+    }
+
+    private fun importQrCode(filename: String) =
+        File("../../../proton-authenticator/test_data/qr/${filename}").readBytes()
+}

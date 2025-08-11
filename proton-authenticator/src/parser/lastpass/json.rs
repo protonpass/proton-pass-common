52b21d01f4c05cbcdfae28a99088e53ac2816d26
Carlos Quintana <carlos.quintana@proton.ch>
2025-08-11 13:21:51 +0200

    style: fix fmt

--- proton-authenticator/src/parser/lastpass/json.rs
+++ proton-authenticator/src/parser/lastpass/json.rs
@@ -132,7 +132,14 @@ mod test {
         }
         match &entries[2].content {
             AuthenticatorEntryContent::Totp(totp) => {
-                check_totp(totp, Algorithm::SHA512, 6, 30, Some("sha512 name".to_string()), Some("sha512 name".to_string()));
+                check_totp(
+                    totp,
+                    Algorithm::SHA512,
+                    6,
+                    30,
+                    Some("sha512 name".to_string()),
+                    Some("sha512 name".to_string()),
+                );
             }
             _ => panic!("Should be a TOTP"),
         }

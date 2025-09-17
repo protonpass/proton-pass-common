89389a1e33fb7a0449edd857134adca5a2eec1c1
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-09-17 09:02:43 +0200

    (cargo-release) Bump version

--- proton-authenticator-mobile/android/lib/build.gradle.kts
+++ proton-authenticator-mobile/android/lib/build.gradle.kts
@@ -59,7 +59,7 @@ android {
 
 mavenPublishing {
     group = "me.proton.authenticator.common"
-    version = "0.25.11"
+    version = "0.25.12"
     pom {
         scm {
             connection.set(gitHubDomain)
--- proton-pass-mobile/android/lib/build.gradle.kts
+++ proton-pass-mobile/android/lib/build.gradle.kts
@@ -60,7 +60,7 @@ android {
 
 mavenPublishing {
     group = "me.proton.pass.common"
-    version = "0.25.11"
+    version = "0.25.12"
     pom {
         scm {
             connection.set(gitHubDomain)

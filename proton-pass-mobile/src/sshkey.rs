0894625a14d56835f46f2f6a25ece6ba9a410f82
Carlos Quintana <carlos.quintana@proton.ch>
2025-10-27 16:29:00 +0100

    feat(pass-mobile): expose SSH key functions

---
+++ proton-pass-mobile/src/sshkey.rs
@@ -0,0 +1,84 @@
+use proton_pass_common::sshkey::{
+    generate_ssh_key as common_generate_ssh_key, validate_private_key as common_validate_private_key,
+    validate_public_key as common_validate_public_key, SshKeyError as CommonSshKeyError,
+    SshKeyPair as CommonSshKeyPair, SshKeyType as CommonSshKeyType,
+};
+
+#[derive(Debug, proton_pass_derive::Error)]
+pub enum SshKeyError {
+    InvalidPublicKey(String),
+    InvalidPrivateKey(String),
+    GenerationFailed(String),
+}
+
+impl From<CommonSshKeyError> for SshKeyError {
+    fn from(e: CommonSshKeyError) -> Self {
+        match e {
+            CommonSshKeyError::InvalidPublicKey(s) => SshKeyError::InvalidPublicKey(s),
+            CommonSshKeyError::InvalidPrivateKey(s) => SshKeyError::InvalidPrivateKey(s),
+            CommonSshKeyError::GenerationFailed(s) => SshKeyError::GenerationFailed(s),
+        }
+    }
+}
+
+type Result<T> = std::result::Result<T, SshKeyError>;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum SshKeyType {
+    RSA2048,
+    RSA4096,
+    Ed25519,
+}
+
+impl From<SshKeyType> for CommonSshKeyType {
+    fn from(other: SshKeyType) -> Self {
+        match other {
+            SshKeyType::RSA2048 => CommonSshKeyType::RSA2048,
+            SshKeyType::RSA4096 => CommonSshKeyType::RSA4096,
+            SshKeyType::Ed25519 => CommonSshKeyType::Ed25519,
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct SshKeyPair {
+    pub public_key: String,
+    pub private_key: String,
+}
+
+impl From<CommonSshKeyPair> for SshKeyPair {
+    fn from(other: CommonSshKeyPair) -> Self {
+        Self {
+            public_key: other.public_key,
+            private_key: other.private_key,
+        }
+    }
+}
+
+pub struct SshKeyManager;
+
+impl SshKeyManager {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn validate_public_key(&self, key: String) -> Result<()> {
+        Ok(common_validate_public_key(&key)?)
+    }
+
+    pub fn validate_private_key(&self, key: String) -> Result<()> {
+        Ok(common_validate_private_key(&key)?)
+    }
+
+    pub fn generate_ssh_key(
+        &self,
+        name: String,
+        email: String,
+        key_type: SshKeyType,
+        passphrase: Option<String>,
+    ) -> Result<SshKeyPair> {
+        let common_key_type = CommonSshKeyType::from(key_type);
+        let result = common_generate_ssh_key(name, email, common_key_type, passphrase)?;
+        Ok(SshKeyPair::from(result))
+    }
+}

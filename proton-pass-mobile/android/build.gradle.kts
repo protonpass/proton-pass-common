3e1c3e95233cde47e7a7b2505cbd868991fd2b7b
Victor <victor.hidalgo@proton.ch>
2026-04-21 10:14:28 +0200

    chore: update agp

--- proton-pass-mobile/android/build.gradle.kts
+++ proton-pass-mobile/android/build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("org.jetbrains.kotlin.jvm") version "2.0.21" apply false
     id("org.jetbrains.kotlinx.atomicfu") version "0.27.0" apply false
